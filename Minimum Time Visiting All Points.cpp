6f243b80a99c256eef1733efc3c14040015095d0
Nesma Hesham <nesmaahh1@gmail.com>
2026-01-12 13:22:56 +0200

    Add solution for minimum time visiting all points
    Implement the minTimeToVisitAllPoints function to calculate the minimum time to visit all points in a 2D plane.

--- Minimum Time Visiting All Points.cpp
+++ Minimum Time Visiting All Points.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+    // problem link --> https://leetcode.com/problems/minimum-time-visiting-all-points/?envType=daily-question&envId=2026-01-12
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& p) {
+        int sum=0; 
+        for(int i=1; i<p.size(); i++){
+            sum+=max(abs(p[i][0]-p[i-1][0]),abs(p[i][1]-p[i-1][1]));
+        }
+        return sum;
+        
+    }
+};
